62c37461e275d2665d267745e2005245a7d2e779
Jason Versteeg <114743484+JasonEdgerunner@users.noreply.github.com>
2023-03-31 10:41:54 +0200

    Finished variable assignments 1 through 4

---
+++ CSharpOpdrachten/VariableOpdracht1/VariableOpdracht1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VariableOpdracht1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Character stats (name, hp and strength respectively.)");
+
+            //maak hier 3 local variables aan:
+            //1 type=string, naam: name, waarde je favoriete rpg character
+            string name = "Sephiroth";
+            //2 type=int, naam: hp, een waarde van hoeveel health dit character heeft
+            int hp = 400000;
+            //3 type=int, naam: str, een waarde van hoe sterk dit character is
+            int str = 246;
+
+
+            //nu zou de statement hieronder moeten werken
+            Console.WriteLine($"{name} {hp} {str}");
+        }
+    }
+
+}
---
+++ CSharpOpdrachten/VariableOpdracht2/VariableOpdracht2/TwitchStream.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VariableOpdracht2
+{
+    internal class TwitchStream
+    {
+        //maak hier class variables aan:
+        string variables = "variables";
+        
+            // type=string, naam: name,
+            string name = "Korbosnapped";
+            // type=string, naam: lastStreamName
+            string lastStreamName = "Just Slaying, talking shite as per usual";
+            // type=Game, naam: streamGame
+            string streamGame = "Just Chatting";
+            // type=int, naam: followers
+            int followers = 1100;
+            // type=bool, naam: automod
+            bool automod = true;
+        
+    }
+}
