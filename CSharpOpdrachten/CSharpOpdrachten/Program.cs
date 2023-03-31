449cbae2fb306337d7810c2d8f9e1d33bae9c34d
Jason Versteeg <114743484+JasonEdgerunner@users.noreply.github.com>
2023-03-31 09:37:58 +0200

    Should be added as intended

---
+++ CSharpOpdrachten/CSharpOpdrachten/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharpOpdrachten
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Op je muil, gauw.");
+            Console.ReadLine();
+        }
+    }
+}
